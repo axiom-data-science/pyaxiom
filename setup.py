260911a0a46601092aa75882c806ca921a0cbf6d
Kyle Wilcox <kyle@axiomalaska.com>
2014-12-10 15:51:16 -0500

    Add setup.py file so we can install

---
+++ setup.py
@@ -0,0 +1,51 @@
+from __future__ import with_statement
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+version = "0.0.1-dev"
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+reqs = [line.strip() for line in open('requirements.txt')]
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+setup(
+    name                = "pyaxiom",
+    version             = version,
+    description         = "A library to manage various Axiom assets using Python",
+    long_description    = readme(),
+    license             = 'LGPLv3',
+    author              = "Kyle Wilcox",
+    author_email        = "kyle@axiomalaska.com",
+    url                 = "https://git.axiom/axiom/pyncml",
+    packages            = find_packages(),
+    install_requires    = reqs,
+    tests_require       = ['pytest'],
+    cmdclass            = {'test': PyTest},
+    classifiers         = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+    ],
+    include_package_data = True,
+)
