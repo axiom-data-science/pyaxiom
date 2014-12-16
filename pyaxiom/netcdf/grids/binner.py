f1cae08b570703e915f06172b0bbbb54f6d96150
Kyle Wilcox <kyle@axiomalaska.com>
2014-12-16 09:50:29 -0500

    Fix logging message when combining through `binner`

--- pyaxiom/netcdf/grids/binner.py
+++ pyaxiom/netcdf/grids/binner.py
@@ -56,7 +56,7 @@ def main(output_path, delta, ncml_file=None, glob_string=None, apply_to_members=
             file_name = "{0}_TO_{1}.nc".format(starting, ending)
         output_file = os.path.join(output_path, file_name)
 
-        pyaxiomlogger.info("Combining ({0}/{1}) - {3} files into {1}".format(i+1, len(windows), len(window.members), output_file))
+        pyaxiomlogger.info("Combining ({0}/{1}) - {2} files into {3}".format(i+1, len(windows), len(window.members), output_file))
         Collection.combine(members=window.members, output_file=output_file)
 
     return 0
