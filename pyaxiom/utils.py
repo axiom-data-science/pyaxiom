da7984775cf8d3e619677f1591c5098325169d9e
Kyle Wilcox <kyle@axiomalaska.com>
2015-02-16 15:57:04 -0500

    Use IoosUrn in the `urnify` method, and support bounds and vertical_datum

--- pyaxiom/utils.py
+++ pyaxiom/utils.py
@@ -1,6 +1,8 @@
 #!python
 # coding=utf-8
 
+from pyaxiom.urn import IoosUrn
+from pyaxiom import logger
 
 class DotDict(object):
     def __init__(self, *args, **kwargs):
@@ -13,14 +15,29 @@ class DotDict(object):
 
 
 def urnify(naming_authority, station_identifier, data_variable):
-    if hasattr(data_variable, 'standard_name)'):
-        variable = data_variable.standard_name
-    else:
-        variable = data_variable.name
+    extras = []
 
-    if hasattr(data_variable, 'cell_methods'):
+    if hasattr(data_variable, 'cell_methods') and data_variable.cell_methods:
         split_cms = data_variable.cell_methods.split(":")
         cms = [ "{0}:{1}".format(c[0].strip(), c[1].strip()) for c in zip(split_cms[0::2], split_cms[1::2]) ]
-        variable = "{0}#cell_methods={1}".format(variable, ",".join(cms))
+        extras.append('cell_methods={0}'.format(','.join(cms)))
 
-    return "urn:ioos:sensor:{!s}:{!s}:{!s}".format(naming_authority, station_identifier, variable).lower().replace(" ", "_")
+    if hasattr(data_variable, 'bounds') and data_variable.bounds:
+        extras.append('bounds={0}'.format(data_variable.bounds))
+
+    if hasattr(data_variable, 'vertical_datum') and data_variable.vertical_datum:
+        extras.append('vertical_datum={0}'.format(data_variable.vertical_datum))
+
+    variable_name = data_variable.name
+    if hasattr(data_variable, 'standard_name') and data_variable.standard_name:
+        variable_name = data_variable.standard_name
+
+    if extras:
+        variable_name = '{0}#{1}'.format(variable_name, ';'.join(extras))
+
+    u = IoosUrn(asset_type='sensor',
+                authority=naming_authority,
+                label=station_identifier,
+                component=variable_name,
+                version=None)
+    return u.urn
