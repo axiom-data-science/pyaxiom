71f8a3de884aa64505c42c7c82706a130ab7d0b9
Kyle Wilcox <kyle@axiomalaska.com>
2016-07-12 11:59:13 -0400

    Ok how about I test PY2 locally.

--- pyaxiom/utils.py
+++ pyaxiom/utils.py
@@ -32,7 +32,12 @@ def unique_justseen(iterable, key=None):
     "List unique elements, preserving order. Remember only the element just seen."
     # unique_justseen('AAAABBBCCDAABBB') --> A B C D A B
     # unique_justseen('ABBCcAD', str.lower) --> A B C A D
-    from builtins import map
+    try:
+        # PY2 support
+        from itertools import imap as map
+    except ImportError:
+        from builtins import map
+
     return map(next, map(operator.itemgetter(1), itertools.groupby(iterable, key)))
 
 
