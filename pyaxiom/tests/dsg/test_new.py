4fb141ee19bfff73d2cb95694196d04321cf6c78
Kyle Wilcox <kyle@axiomalaska.com>
2016-07-12 11:01:42 -0400

    Use py.test parametize rather that subTest which is only Py>=3.4

--- pyaxiom/tests/dsg/test_new.py
+++ pyaxiom/tests/dsg/test_new.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
-import unittest
+
+import pytest
 
 from pyaxiom.netcdf import CFDataset
 from pyaxiom.utils import all_subclasses
@@ -12,33 +13,20 @@ logger.level = logging.DEBUG
 logger.handlers = [logging.StreamHandler()]
 
 
-class TestIsMine(unittest.TestCase):
-
-    def test_is_mine(self):
-
-        tests = {
-            OrthogonalMultidimensionalProfile: [
-                os.path.join(os.path.dirname(__file__), 'profile', 'resources', 'om-single.nc'),
-                os.path.join(os.path.dirname(__file__), 'profile', 'resources', 'om-multiple.nc')
-            ],
-            IncompleteMultidimensionalProfile: [
-                os.path.join(os.path.dirname(__file__), 'profile', 'resources', 'im-multiple.nc')
-            ],
-            IncompleteMultidimensionalTrajectory: [
-                os.path.join(os.path.dirname(__file__), 'trajectory', 'resources', 'im-single.nc'),
-                os.path.join(os.path.dirname(__file__), 'trajectory', 'resources', 'im-multiple.nc')
-            ]
-        }
-
-        allsubs = list(all_subclasses(CFDataset))
-        for klass, file_list in tests.items():
-            subs = [ s for s in allsubs if s != klass ]
-            for fp in file_list:
-                dsg = CFDataset(fp)
-                logger.info('Testing {} - {}...'.format(klass.__name__, fp))
-                assert klass.is_mine(dsg) is True
-                with self.subTest(msg='{} - {}'.format(klass.__name__, fp), dsg=dsg, klass=klass):
-                    for s in subs:
-                        if hasattr(s, 'is_mine'):
-                            logger.info('  * Trying {}...'.format(s.__name__))
-                            assert s.is_mine(dsg) is False
+@pytest.mark.parametrize("klass,fp", [
+    (OrthogonalMultidimensionalProfile,         os.path.join(os.path.dirname(__file__), 'profile', 'resources', 'om-single.nc')),
+    (OrthogonalMultidimensionalProfile,         os.path.join(os.path.dirname(__file__), 'profile', 'resources', 'om-multiple.nc')),
+    (IncompleteMultidimensionalProfile,         os.path.join(os.path.dirname(__file__), 'profile', 'resources', 'im-multiple.nc')),
+    (IncompleteMultidimensionalTrajectory,      os.path.join(os.path.dirname(__file__), 'trajectory', 'resources', 'im-single.nc')),
+    (IncompleteMultidimensionalTrajectory,      os.path.join(os.path.dirname(__file__), 'trajectory', 'resources', 'im-multiple.nc')),
+])
+def test_is_mine(klass, fp):
+    allsubs = list(all_subclasses(CFDataset))
+    subs = [ s for s in allsubs if s != klass ]
+    dsg = CFDataset(fp)
+    logger.info('\nTesting {}'.format(klass.__name__))
+    assert klass.is_mine(dsg) is True
+    for s in subs:
+        if hasattr(s, 'is_mine'):
+            logger.info('  * Trying {}...'.format(s.__name__))
+            assert s.is_mine(dsg) is False
