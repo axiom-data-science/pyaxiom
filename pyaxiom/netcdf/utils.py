5386edbd7c88a1f53c88869abbf63c00ce212352
Kyle Wilcox <kyle@axiomalaska.com>
2016-08-02 17:37:17 -0400

    Fix cf_safe_name for python 2.7

--- pyaxiom/netcdf/utils.py
+++ pyaxiom/netcdf/utils.py
@@ -2,9 +2,16 @@
 # coding=utf-8
 
 
+def isstr(s):
+    try:
+        return isinstance(s, basestring)
+    except NameError:
+        return isinstance(s, str)
+
+
 def cf_safe_name(name):
     import re
-    if isinstance(name, str):
+    if isstr(name):
         if re.match('^[0-9_]', name):
             # Add a letter to the front
             name = "v_{}".format(name)
